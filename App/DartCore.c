f87df339ef3924a15056f10415dd4954a7b0ef44
FHb212 <23009101431@stu.xidian.edu.cn>
2025-10-16 19:23:30 +0800

    new:控制roll轴

--- App/DartCore.c
+++ App/DartCore.c
@@ -2,10 +2,50 @@
 #include "DataGet.h"
 #include "arm_math.h"
 #include "pid.h"
+#include "flycontrol.h"
 
+extern Dart_data dart_data;
+float yaw_setpoint = 0.0f;
+float roll_setpoint = 0.0f;
+float pitch_setpoint = 0.0f;
+float LU_angle, RU_angle, RD_angle, LD_angle;
 PID_Config_t pitch_angle_pid = {8.0f, 0.0f, 0.2f, 300.0f, 100.0f};
 PID_Config_t yaw_angle_pid = {8.0f, 0.0f, 0.2f, 300.0f, 100.0f};
 PID_Config_t pitch_rate_pid = {0.15f, 0.0f, 0.0f, 300.0f, 100.0f};
 PID_Config_t yaw_rate_pid = {0.15f, 0.0f, 0.0f, 300.0f, 100.0f};
 PID_Config_t roll_angle_pid = {8.0f, 0.0f, 0.2f, 300.0f, 100.0f};
 PID_Config_t roll_rate_pid = {0.15f, 0.0f, 0.0f, 300.0f, 100.0f};
+
+void DartCore_XWing_Mix(float Mx, float My, float Mz,
+                        float *LU, float *RU, float *RD, float *LD)
+{
+    float theta = DEG2RAD(45.0f);
+    float c = cosf(theta);
+    float s = sinf(theta);
+    // RU: +cosθ *Mx  -sinθ *My  +sinθ *Mz
+    if (RU)
+        *RU = Mx * c - My * s + Mz * s;
+    // RD: +cosθ *Mx  +sinθ *My  -sinθ *Mz
+    if (RD)
+        *RD = Mx * c + My * s - Mz * s;
+    // LU: -cosθ *Mx  -sinθ *My  -sinθ *Mz
+    if (LU)
+        *LU = -Mx * c - My * s - Mz * s;
+    // LD: -cosθ *Mx  +sinθ *My  +sinθ *Mz
+    if (LD)
+        *LD = -Mx * c + My * s + Mz * s;
+}
+
+void Dart_control_loop()
+{
+    Dart_control_roll(dart_data.imu.Roll, dart_data.imu.gyro_z);
+}
+
+void Dart_control_roll(float roll_angle_now, float roll_rate_now)
+{
+    float roll_set_rate;
+    roll_set_rate = PID_calculate(&roll_angle_pid, roll_angle_now, roll_setpoint);
+    roll_setpoint = PID_calculate(&roll_rate_pid, roll_rate_now, roll_set_rate);
+    DartCore_XWing_Mix(0.0f, roll_setpoint, 0.0f, &LU_angle, &RU_angle, &RD_angle, &LD_angle);
+    FlyControl_SetAngle(LU_angle, RU_angle, RD_angle, LD_angle);
+}
--- App/DartCore.h
+++ App/DartCore.h
@@ -1,4 +1,3 @@
-
 #pragma once
 #include "Flycontrol.h"
 #include <math.h>
@@ -11,48 +10,7 @@ typedef struct
     float max_out;
     float max_iout;
 } PID_Config_t;
-// 左前翼面
-float DartCore_XWing_LU(float yaw, float roll, float pitch)
-{
-    float c45 = cosf(DEG2RAD(45.0f));
-    float s45 = sinf(DEG2RAD(45.0f));
-    float Ky = 1.0f;
-    return pitch * c45 + roll * s45 + yaw * Ky;
-}
+// 合成一个函数，分别输出四个翼面角度到四个变量
 
-// 右前翼面
-float DartCore_XWing_RU(float yaw, float roll, float pitch)
-{
-    float c45 = cosf(DEG2RAD(45.0f));
-    float s45 = sinf(DEG2RAD(45.0f));
-    float Ky = 1.0f;
-    return pitch * c45 - roll * s45 - yaw * Ky;
-}
-
-// 右后翼面
-float DartCore_XWing_RD(float yaw, float roll, float pitch)
-{
-    float c45 = cosf(DEG2RAD(45.0f));
-    float s45 = sinf(DEG2RAD(45.0f));
-    float Ky = 1.0f;
-    return -pitch * c45 - roll * s45 + yaw * Ky;
-}
-
-// 左后翼面L
-float DartCore_XWing_LD(float yaw, float roll, float pitch)
-{
-    float c45 = cosf(DEG2RAD(45.0f));
-    float s45 = sinf(DEG2RAD(45.0f));
-    float Ky = 1.0f;
-    return -pitch * c45 + roll * s45 - yaw * Ky;
-}
-void DartCore_XWing_Reverse(float A, float B, float C, float D, float *yaw, float *roll, float *pitch)
-{
-    float c45 = cosf(DEG2RAD(45.0f));
-    float s45 = sinf(DEG2RAD(45.0f));
-    float Ky = 1.0f;
-
-    *pitch = (A + B - C - D) / (4.0f * c45);
-    *roll = (A - B - C + D) / (4.0f * s45);
-    *yaw = (A - B + C - D) / (4.0f * Ky);
-}
+void DartCore_XWing_Mix(float Mx, float My, float Mz,
+                        float *LU, float *RU, float *RD, float *LD);
